b77ecf25456ce40956ad4161a83d253c17c832d2
DyStorzion <benja2705@gmail.com>
2024-06-12 15:58:06 -0400

    fix: "se arregla logica de recomendacion"

--- API_Recomendaciones/models.py
+++ API_Recomendaciones/models.py
@@ -4,170 +4,13 @@ uri = "mongodb+srv://beespinoza2022:eVsJfxayY1I1586t@cluster0.yblbnqi.mongodb.ne
 client = MongoClient(uri)
 db = client["proyectoSemestralDis"]
 collectionSitios = db["sitios"]
-
-if "sitios" not in db.list_collection_names():
-    try:
-        db.create_collection("sitios", validator=
-                             {
-          "$jsonSchema": {
-            "bsonType": "object",
-            "required": ["nombre", "descripcion", "latitud", "longitud", "categorias", "estado", "fecha_creacion", "usuario_creo"],
-            "properties": {
-              "nombre": {
-                "bsonType": "string",
-                "description": "El nombre del sitio debe ser una cadena de texto y es obligatorio"
-              },
-              "descripcion": {
-                "bsonType": "string",
-                "description": "La descripción del sitio debe ser una cadena de texto y es obligatoria"
-              },
-              "latitud": {
-                "bsonType": "double",
-                "description": "La latitud del sitio debe ser un número de punto flotante y es obligatoria"
-              },
-              "longitud": {
-                "bsonType": "double",
-                "description": "La longitud del sitio debe ser un número de punto flotante y es obligatoria"
-              },
-              "categorias": {
-                "bsonType": "array",
-                "items": {
-                  "bsonType": "string"
-                },
-                "description": "Las categorías del sitio deben ser un array de cadenas de texto"
-              },
-              "calificacion_promedio": {
-                "bsonType": "double",
-                "description": "La calificación promedio del sitio debe ser un número de punto flotante"
-              },
-              "reseñas": {
-                "bsonType": "array",
-                "items": {
-                  "bsonType": "object",
-                  "required": ["usuario", "calificacion", "comentario", "fecha"],
-                  "properties": {
-                    "usuario": {
-                      "bsonType": "string",
-                      "description": "El nombre del usuario que hizo la reseña debe ser una cadena de texto y es obligatorio"
-                    },
-                    "calificacion": {
-                      "bsonType": "double",
-                      "description": "La calificación de la reseña debe ser un número de punto flotante y es obligatoria"
-                    },
-                    "comentario": {
-                      "bsonType": "string",
-                      "description": "El comentario de la reseña debe ser una cadena de texto y es obligatorio"
-                    },
-                    "fecha": {
-                      "bsonType": "date",
-                      "description": "La fecha de la reseña debe ser una fecha y es obligatoria"
-                    }
-                  }
-                },
-                "description": "Las reseñas del sitio deben ser un array de objetos"
-              },
-              "estado": {
-                "bsonType": "string",
-                "enum": ["activo", "inactivo"],
-                "description": "El estado del sitio debe ser 'activo' o 'inactivo' y es obligatorio"
-              },
-              "fecha_creacion": {
-                "bsonType": "date",
-                "description": "La fecha de creación del sitio debe ser una fecha y es obligatoria"
-              },
-              "usuario_creo": {
-                "bsonType": "string",
-                "description": "El usuario que creó el sitio debe ser una cadena de texto y es obligatorio"
-              },
-              "fecha_modificacion": {
-                "bsonType": "date",
-                "description": "La fecha de la última modificación del sitio debe ser una fecha"
-              },
-              "usuario_modifico": {
-                "bsonType": "string",
-                "description": "El usuario que realizó la última modificación debe ser una cadena de texto"
-              },
-              "fecha_ultimo_ingreso": {
-                "bsonType": "date",
-                "description": "La fecha del último ingreso al sitio debe ser una fecha"
-              }
-            }
-          }
-        })
-    except errors.CollectionInvalid:
-        pass
-
 collectionUsuarios = db["usuarios"]
 
-if "usuarios" not in db.list_collection_names():
-    try:
-        db.create_collection("usuarios", validator=
-            {
-            "$jsonSchema": {
-                "bsonType": "object",
-                "required": ["nombre", "email", "estado", "usuario_creacion", "es_administrador"],
-                "properties": {
-                  "nombre": {
-                    "bsonType": "string",
-                    "description": "El nombre del usuario debe ser una cadena de texto y es obligatorio"
-                  },
-                  "email": {
-                    "bsonType": "string",
-                    "description": "El correo electrónico del usuario debe ser una cadena de texto y es obligatorio"
-                  },
-                  "preferencias": {
-                    "bsonType": "array",
-                    "items": {
-                      "bsonType": "string"
-                    },
-                    "description": "Las preferencias del usuario deben ser un array de cadenas de texto"
-                  },
-                  "fecha_nacimiento": {
-                    "bsonType": "date",
-                    "description": "La fecha de nacimiento del usuario debe ser una fecha"
-                  },
-                  "estado": {
-                    "bsonType": "string",
-                    "enum": ["activo", "inactivo"],
-                    "description": "El estado del usuario debe ser 'activo' o 'inactivo' y es obligatorio"
-                  },
-                  "fecha_creacion": {
-                    "bsonType": "date",
-                    "description": "La fecha de creación del usuario debe ser una fecha y es obligatorio"
-                  },
-                  "usuario_creacion": {
-                    "bsonType": "string",
-                    "description": "El usuario que creó el usuario debe ser una cadena de texto y es obligatorio"
-                  },
-                  "fecha_ultima_modificacion": {
-                    "bsonType": "date",
-                    "description": "La fecha de la última modificación del usuario debe ser una fecha"
-                  },
-                  "usuario_modifico": {
-                    "bsonType": "string",
-                    "description": "El usuario que realizó la última modificación debe ser una cadena de texto"
-                  },
-                  "fecha_ultimo_ingreso": {
-                    "bsonType": "date",
-                    "description": "La fecha del último ingreso del usuario debe ser una fecha"
-                  },
-                  "es_administrador": {
-                    "bsonType": "bool",
-                    "description": "Indica si el usuario es administrador y es obligatorio"
-                  }
-                }
-              }
-            }
-        )
-    except errors.CollectionInvalid:
-        pass
-
-    
 class Usuario:
-    def __init__(self, user_id, nombre, sitios_visitados):
+    def __init__(self, user_id, nombre, preferencias):
         self.user_id = user_id
         self.nombre = nombre
-        self.sitios_visitados = sitios_visitados
+        self.preferencias = preferencias
     
     @classmethod
     def from_json(cls, json_data):
@@ -175,36 +18,41 @@ class Usuario:
             return cls(
                 user_id = json_data.get('_id'),
                 nombre = json_data.get('name'),
-                sitios_visitados = json_data.get('visited_sites', [])
+                preferencias = json_data.get('preferencias', [])
             )
         return None
     
+    @classmethod
+    def find_by_id(cls, user_id):
+        usuario = collectionUsuarios.find_one({'_id': user_id})
+        if usuario:
+            return Usuario(usuario['_id'], usuario['name'], usuario['preferencias'])
+        return None
+    
 class Sitio:
-    def __init__(self, nombre_sitio, latitud, longitud, descripcion=None, tags=None, categoria=None, popularidad=0, rating=0.0, horario_apertura=None, direccion=None):
+    def __init__(self, nombre_sitio, latitud, longitud, descripcion=None, categorias = None, calificacion_promedio=None):
         self.nombre_sitio = nombre_sitio
         self.latitud = latitud
         self.longitud = longitud
         self.descripcion = descripcion
-        self.tags = tags if tags is not None else []
-        self.categoria = categoria
-        self.popularidad = popularidad
-        self.rating = rating
-        self.horario_apertura = horario_apertura
-        self.direccion = direccion
+        self.categorias = categorias
+        self.calificacion_promedio = calificacion_promedio
 
     @classmethod
     def from_json(cls, json_data):
+        if(json_data is None):
+            return None
+        nombre = json_data.get('nombre')
+        sitio = collectionSitios.find_one({'nombre': nombre})
+        if(sitio is None):
+            return None
         return cls(
-            nombre_sitio=json_data.get('nombre_sitio'),
-            latitud=json_data.get('latitud'),
-            longitud=json_data.get('longitud'),
-            descripcion=json_data.get('descripcion'),
-            tags=json_data.get('tags', []),
-            categoria=json_data.get('categoria'),
-            popularidad=json_data.get('popularidad', 0),
-            rating=json_data.get('rating', 0.0),
-            horario_apertura=json_data.get('horario_apertura'),
-            direccion=json_data.get('direccion')
+            nombre_sitio = sitio.get('nombre'),
+            latitud = sitio.get('latitud'),
+            longitud = sitio.get('longitud'),
+            descripcion = sitio.get('descripcion'),
+            categorias = sitio.get('categorias'),
+            calificacion_promedio = sitio.get('calificacion_promedio'),
         )
 
     def to_json(self):
@@ -213,10 +61,65 @@ class Sitio:
             'latitud': self.latitud,
             'longitud': self.longitud,
             'descripcion': self.descripcion,
-            'tags': self.tags,
-            'categoria': self.categoria,
-            'popularidad': self.popularidad,
-            'rating': self.rating,
-            'horario_apertura': self.horario_apertura,
-            'direccion': self.direccion
+            'categorias': self.categorias,
+        }
+    
+    @classmethod
+    def find_by_name(cls, nombre_sitio):
+        sitio = collectionSitios.find_one({'nombre': nombre_sitio})
+        if sitio:
+            return Sitio(sitio['nombre'], sitio['latitud'], sitio['longitud'], sitio['descripcion'], sitio['categorias'])
+        return None
+    
+class Categoria ():
+    def __init__(self, nombre_categoria, descripcion):
+        self.nombre_categoria = nombre_categoria
+        self.descripcion = descripcion
+
+    @classmethod
+    def from_json(cls, json_data):
+        return cls(
+            nombre_categoria=json_data.get('nombre_categoria'),
+            descripcion=json_data.get('descripcion')
+        )
+
+    def to_json(self):
+        return {
+            'nombre_categoria': self.nombre_categoria,
+            'descripcion': self.descripcion
+        }
+    
+class Recomendacion ():
+    def __init__(self, usuario, sitio):
+        self.usuario = usuario
+        self.sitio = sitio
+    def to_json(self):
+        return {
+            self.usuario.to_json(),
+            self.sitio.to_json()
+        }
+
+
+class Review ():
+    def __init__(self, user_id, sitio_id, calificacion, comentario):
+        self.user_id = user_id
+        self.sitio_id = sitio_id
+        self.calificacion = calificacion
+        self.comentario = comentario
+
+    @classmethod
+    def from_json(cls, json_data):
+        return cls(
+            user_id=json_data.get('user_id'),
+            sitio_id=json_data.get('sitio_id'),
+            calificacion=json_data.get('calificacion'),
+            comentario=json_data.get('comentario')
+        )
+
+    def to_json(self):
+        return {
+            'user_id': self.user_id,
+            'sitio_id': self.sitio_id,
+            'calificacion': self.calificacion,
+            'comentario': self.comentario
         }
--- API_Recomendaciones/recomendador.py
+++ API_Recomendaciones/recomendador.py
@@ -1,20 +1,29 @@
-from models import Usuario, Sitio, collectionSitios, collectionUsuarios
+from models import Usuario, Sitio, Recomendacion, Review, collectionSitios, collectionUsuarios
 
 class SistemaRecomendacion:
     def __init__(self, json_sitio, json_usuario = None):
         self.sitio_actual = Sitio.from_json(json_sitio)
         self.usuario = Usuario.from_json(json_usuario)
+        self.recomendaciones = []
 
     def generar_recomendacion(self):
         sitios_recomendados = collectionSitios.find({
-            'tags': {'$in': self.sitio_actual.tags},
-            #'categoria': self.sitio_actual.categoria,
-            'nombre_sitio': {'$ne': self.sitio_actual.nombre_sitio}
-        }).sort([('rating', -1), ('popularidad', -1)]).limit(5)
+            'categorias': {'$in': self.sitio_actual.categorias},
+            'nombre': {'$ne': self.sitio_actual.nombre_sitio}
+        }).sort([('calificacion_promedio', -1)]).limit(5)
+        
+        sitios_recomendados = list(sitios_recomendados)
+        
+        for sitio in sitios_recomendados:
+            sitioObj = Sitio.find_by_name(sitio['nombre'])
+            self.recomendaciones.append(Recomendacion(self.usuario, sitioObj))
 
-        recomendaciones = []
-        for site in sitios_recomendados:
-            site_obj = Sitio.from_json(site)
-            recomendaciones.append(site_obj.to_json())
+        retorno = []
+        for recomendacion in self.recomendaciones:
+            site_json = recomendacion.sitio.to_json()
+            retorno.append(site_json)
 
-        return recomendaciones
+        return retorno
+
+    def get_recomendaciones(self):
+        return self.recomendaciones
